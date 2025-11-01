249851667cfeb744941b7f07a200fd8f990e1b88
lemon <lemon@example.com>
2025-11-01 19:24:44 +0300

    commit

--- ai-chat-django/mermind/apps.py
+++
@@ -1,8 +0,0 @@
-# mermind/apps.py
-from django.apps import AppConfig
-
-class MermindConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "mermind"
-    def ready(self):
-        from . import signals  # noqa
